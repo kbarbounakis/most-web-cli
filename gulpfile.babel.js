2bbb56b47e6797694f9ad956c4f62c4f8e4a5473
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2018-04-07 14:31:37 +0000

    initial commit of v2.0.0

---
+++ gulpfile.babel.js
@@ -0,0 +1,47 @@
+/*eslint no-var: "off"*/
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var eslint = require('gulp-eslint');
+const child_process = require('child_process');
+
+var modules = [
+  'src/**/*.js'
+];
+
+function lint(files, options) {
+  return function() {
+    return gulp.src(files)
+      .pipe(eslint(options))
+      .pipe(eslint.format())
+  };
+}
+
+function build(files) {
+  return function () {
+    return gulp.src(files)
+      .pipe(sourcemaps.init())
+      .pipe(babel())
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('dist'));
+  }
+}
+
+// test
+gulp.task('build', ['lint'], build(modules));
+//lint test
+gulp.task('lint', lint(modules));
+
+gulp.task('watch', ['build'], function () {
+  var files = [].concat(modules);
+    //watch for server module changes
+  gulp.watch(files, function(file) {
+    gutil.log(gutil.colors.green('Compiling ' + file.path));
+    return build(file.path)();
+  });
+});
+
+gulp.task('default', function() {
+  gulp.start('build');
+});
