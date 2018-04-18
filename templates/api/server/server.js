556cda08fe3ea10ea8719f5be5fc24b75a673fa9
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2018-04-18 22:48:50 +0300

    updates application templates routes

--- templates/api/server/server.js
+++ templates/api/server/server.js
@@ -1,7 +1,16 @@
 import {HttpApplication} from '@themost/web/app';
 import path from 'path';
-
+import {ODataModelBuilderConfiguration} from '@themost/web/odata';
+//initialize app
 let app = new HttpApplication(path.resolve(__dirname));
+//configure api
+ODataModelBuilderConfiguration.config(app).then((builder)=> {
+    builder.hasContextLink((context)=> {
+       return '/api/$metadata';
+    });
+}).catch((err)=> {
+    TraceUtils.error(err);
+});
 //start http application
 app.start({
     port:process.env.PORT ? process.env.PORT: 3000,
