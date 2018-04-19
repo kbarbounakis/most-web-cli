fffd151d796b964d2b0bb9f3af0112da1be73f4f
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2018-04-19 06:29:06 +0000

    application template update

--- templates/api/server/server.js
+++ templates/api/server/server.js
@@ -1,8 +1,11 @@
 import {HttpApplication} from '@themost/web/app';
 import path from 'path';
 import {ODataModelBuilderConfiguration} from '@themost/web/odata';
+import {TraceUtils} from '@themost/common/utils';
 //initialize app
 let app = new HttpApplication(path.resolve(__dirname));
+//set static content
+app.useStaticContent(path.resolve('./app'));
 //configure api
 ODataModelBuilderConfiguration.config(app).then((builder)=> {
     builder.hasContextLink((context)=> {
