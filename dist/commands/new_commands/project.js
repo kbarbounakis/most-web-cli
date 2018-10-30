46473a3a862d015ed8643f0b5564cd512b717aba
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2018-10-30 17:36:51 +0000

    updates express template

--- dist/commands/new_commands/project.js
+++ dist/commands/new_commands/project.js
@@ -11,24 +11,28 @@ var _path = require('path');
 
 var path = _interopRequireDefault(_path).default;
 
+var _crypto = require('crypto');
+
+var crypto = _interopRequireDefault(_crypto).default;
+
 var _fsExtra = require('fs-extra');
 
 var existsSync = _fsExtra.existsSync;
 var readdirSync = _fsExtra.readdirSync;
 var copy = _fsExtra.copy;
+var readFile = _fsExtra.readFile;
+var writeFile = _fsExtra.writeFile;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-/**
- * @license
- * MOST Web Framework 2.0 Codename Blueshift
- * Copyright (c) 2017, THEMOST LP All rights reserved
- *
- * Use of this source code is governed by an BSD-3-Clause license that can be
- * found in the LICENSE file at https://themost.io/license
- */
-var command = exports.command = 'project <directory>';
-
+var command = exports.command = 'project <directory>'; /**
+                                                        * @license
+                                                        * MOST Web Framework 2.0 Codename Blueshift
+                                                        * Copyright (c) 2017, THEMOST LP All rights reserved
+                                                        *
+                                                        * Use of this source code is governed by an BSD-3-Clause license that can be
+                                                        * found in the LICENSE file at https://themost.io/license
+                                                        */
 var desc = exports.desc = 'Create a new project';
 
 function builder(yargs) {
@@ -72,8 +76,52 @@ function handler(argv) {
             console.error(err);
             return process.exit(1);
         }
-        console.log('Operation was completed successfully');
-        return process.exit(0);
+        Promise.all([updateApplicationConfiguration(argv)]).then(function () {
+            console.log('Operation was completed successfully');
+            return process.exit(0);
+        }).catch(function (err) {
+            console.error('ERROR: An error occurred while executing post operations.');
+            console.error(err);
+            return process.exit(1);
+        });
+    });
+}
+
+function updateApplicationConfiguration(argv) {
+    return new Promise(function (resolve, reject) {
+        var projectRoot = path.resolve(process.cwd(), argv.directory);
+        var appJsonPath = path.resolve(projectRoot, 'server/config/app.json');
+        // generate application key
+        if (existsSync(appJsonPath)) {
+            return readFile(appJsonPath, 'utf8', function (err, str) {
+                if (err) {
+                    return reject(err);
+                }
+                var data = JSON.parse(str);
+                return crypto.randomBytes(48, function (err, buffer) {
+                    if (err) {
+                        return reject(err);
+                    }
+                    // set application key
+                    data.settings.crypto.key = buffer.toString('hex');
+                    return crypto.randomBytes(12, function (err, buffer) {
+                        if (err) {
+                            return reject(err);
+                        }
+                        // set unattended execution account
+                        data.settings.auth.unattendedExecutionAccount = buffer.toString('base64');
+                        // write app.json
+                        return writeFile(appJsonPath, JSON.stringify(data, null, 4), 'utf8', function (err) {
+                            if (err) {
+                                return reject(err);
+                            }
+                            return resolve();
+                        });
+                    });
+                });
+            });
+        }
+        return resolve();
     });
 }
 //# sourceMappingURL=project.js.map
--- src/commands/new_commands/project.js
+++ src/commands/new_commands/project.js
@@ -7,8 +7,10 @@
  * found in the LICENSE file at https://themost.io/license
  */
 import path from 'path';
-import {existsSync, readdirSync, copy} from 'fs-extra';
+import crypto from 'crypto';
+import {existsSync, readdirSync, copy, readFile, writeFile} from 'fs-extra';
 export const command = 'project <directory>';
+
 
 export const desc = 'Create a new project';
 
@@ -53,8 +55,54 @@ export function handler(argv) {
                 console.error(err);
                 return process.exit(1);
             }
-            console.log('Operation was completed successfully');
-            return process.exit(0);
+            Promise.all([
+                updateApplicationConfiguration(argv)
+                ]).then(()=> {
+                    console.log('Operation was completed successfully');
+                    return process.exit(0);
+                }).catch(err => {
+                    console.error('ERROR: An error occurred while executing post operations.');
+                    console.error(err);
+                    return process.exit(1);
+                });
         });
     
+}
+
+function updateApplicationConfiguration(argv) {
+    return new Promise((resolve, reject) => {
+        let projectRoot = path.resolve(process.cwd(), argv.directory);
+        let appJsonPath = path.resolve(projectRoot, 'server/config/app.json');
+        // generate application key
+        if (existsSync(appJsonPath)) {
+            return readFile(appJsonPath, 'utf8', (err, str)=> {
+               if (err) {
+                   return reject(err);
+               } 
+               let data = JSON.parse(str);
+               return crypto.randomBytes(48, (err, buffer) => {
+                   if (err) {
+                       return reject(err);
+                   }
+                   // set application key
+                  data.settings.crypto.key = buffer.toString('hex');
+                  return crypto.randomBytes(12, (err, buffer) => {
+                       if (err) {
+                           return reject(err);
+                       }
+                       // set unattended execution account
+                      data.settings.auth.unattendedExecutionAccount = buffer.toString('base64');
+                      // write app.json
+                      return writeFile(appJsonPath, JSON.stringify(data, null, 4), 'utf8', (err) => {
+                          if (err) {
+                              return reject(err);
+                          }
+                          return resolve();
+                      })
+                    });
+                });
+            });
+        }
+        return resolve();
+    });
 }
