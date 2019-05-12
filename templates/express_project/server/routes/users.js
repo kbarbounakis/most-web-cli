6a2f5780c9745f61759ee7f369a8a9d1c7bba925
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2019-05-12 17:42:28 +0300

    update express project and generators

---
+++ templates/express_project/server/routes/users.js
@@ -0,0 +1,15 @@
+import express from 'express';
+let router = express.Router();
+
+/* GET user page. */
+router.get('/me', (req, res, next) => {
+  req.context.model('User')
+      .where('name').equal(req.context.user && req.context.user.name)
+      .getItem().then((user)=> {
+    return res.render('user', user);
+  }).catch( err => {
+    return next(err);
+  });
+});
+
+module.exports = router;
